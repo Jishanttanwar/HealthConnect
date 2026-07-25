e86ca4dbe1105539958c6904508add1a40f0be3a
Jishant tanwar <princetanwar995@gmail.com>
2026-07-25 11:22:05 +0530

    updated the apps urls.py and the config urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/specializations/', include('specializations.urls')),
     path('api/doctors/', include('doctors.urls')),
     path('api/patients/', include('patients.urls')),
+    path("api/appointments/", include("appointments.urls")),
 ]
