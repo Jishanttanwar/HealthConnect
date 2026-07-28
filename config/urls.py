1d3e9a5c9bb57861746059f79eb98841918ebce8
Jishant tanwar <princetanwar995@gmail.com>
2026-07-28 23:16:47 +0530

    completed lab_report modules and added media files configuration

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,12 @@ urlpatterns = [
     path("api/appointments/", include("appointments.urls")),
     path("api/encounters/", include("encounters.urls")),
     path("api/prescriptions/", include("prescriptions.urls")),
+    path("api/lab-reports/", include("lab_reports.urls")),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
