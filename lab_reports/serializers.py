f69eb21e35185883612318793fd2b6ea27e3eac5
Jishant tanwar <princetanwar995@gmail.com>
2026-07-28 23:13:07 +0530

    updated the lab_report file

---
+++ lab_reports/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import LabReport
+
+
+class LabReportSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = LabReport
+        fields = "__all__"
--- lab_reports/views.py
+++ lab_reports/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+from .models import LabReport
+from .serializers import LabReportSerializer
+from .permissions import IsDoctorOrAdmin
+
+
+class LabReportViewSet(viewsets.ModelViewSet):
+
+    queryset = LabReport.objects.select_related(
+        "encounter",
+        "encounter__appointment",
+        "encounter__appointment__doctor",
+        "encounter__appointment__patient",
+    )
+
+    serializer_class = LabReportSerializer
+
+    permission_classes = [IsDoctorOrAdmin]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "status",
+        "report_date",
+    ]
+
+    search_fields = [
+        "report_name",
+        "encounter__appointment__patient__user__username",
+        "encounter__appointment__doctor__user__username",
+    ]
+
+    ordering_fields = [
+        "report_date",
+        "created_at",
+    ]
+
+    ordering = [
+        "-report_date",
+    ]
