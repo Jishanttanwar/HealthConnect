ee759df86eea15f2ffdb8bb9e7c62e62cd4fb36b
Jishant tanwar <princetanwar995@gmail.com>
2026-07-28 23:11:23 +0530

    In lab_report updated admin.py

--- lab_reports/admin.py
+++ lab_reports/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import LabReport
+
+
+@admin.register(LabReport)
+class LabReportAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "report_name",
+        "status",
+        "report_date",
+        "encounter",
+    )
+
+    list_filter = (
+        "status",
+        "report_date",
+    )
+
+    search_fields = (
+        "report_name",
+        "encounter__appointment__patient__user__email",
+        "encounter__appointment__doctor__user__email",
+    )
